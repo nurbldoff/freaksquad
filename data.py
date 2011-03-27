36c1f83dcf57551d96cf6c787c68d776f2bb9502
Johan Forsberg <johan@slentrian.org>
2011-03-28 00:06:12 +0200

    voxel version

--- data.py
+++ data.py
@@ -1,4 +1,8 @@
+from __future__ import division
+
 from random import randint
+import string
+import pygame
 
 class Level(object):
     def __init__(self, size=(10,10,3)):
@@ -13,13 +17,132 @@ class Level(object):
         for z in range(self.zsize):
             self.data.append([])
             for y in range(self.ysize):
-                self.data[z].append([0,]*self.xsize)
+                self.data[z].append([])
+                for x in range(self.xsize):
+                    self.data[z][y].append(Block())
 
     def put_random_block(self, value):
         self.data [randint(0, self.zsize-1)] [randint(0, self.ysize-1)] [randint(0, self.xsize-1)] = value
 
-    def put_block(self, pos, kind):
-        self.data[pos.z][pos.y][pos.x] = kind
+    def put_block(self, pos, block):
+        if type(pos).__name__ == "tuple":
+            self.data[pos[2]][pos[1]][pos[0]] = block
+        else:
+            self.data[pos.z][pos.y][pos.x] = block
 
     def get_block(self, pos):
-        return self.data[pos.z][pos.y][pos.x]
+        if type(pos).__name__ == "tuple":
+            return self.data[pos[2]][pos[1]][pos[0]]
+        else:
+            return self.data[pos.z][pos.y][pos.x]
+
+
+class Block(object):
+    """
+    """
+
+    block_imgs = [pygame.image.load("block2.png"),
+                  pygame.image.load("block2b.png"),
+                  pygame.image.load("block2c.png"),
+                  pygame.image.load("block2d.png")]
+    block_rects = [bl.get_rect() for bl in block_imgs]
+    hz = 32
+    wx = 16
+    hx = 16
+    wy = 32
+    hy = 8
+
+    def __init__(self, tree=0):
+        self.tree = tree
+
+    def draw(self, surface):
+        draw_octree(surface, self.block_imgs, (self.wx, self.hx, self.wy, self.hy, self.hz),
+                    self.tree, 0)
+
+    def put_sub_element(self, scale, position, kind):
+        if self.tree == 0:
+            self.tree = [0,0,0,0,0,0,0,0]
+        elif self.tree == 1:
+            return   #nothing to be done!
+        tree = self.tree
+        x, y, z = position
+        print "scale:", scale
+        for i in range(scale,-1,-1):
+            d = 2**i
+            lx = x // d
+            ly = y // d
+            lz = z // d
+            x -= lx*d
+            y -= ly*d
+            z -= lz*d
+            n = lz*4+ly*2+lx
+            print "n=",n
+
+            if i == 0:
+                tree[n] = kind
+            else:
+                if tree[n] == 0:
+                    tree[n] = tmp = [0,0,0,0,0,0,0,0]
+                    tree = tmp
+                elif tree[n] == 1:
+                    pass
+                else:
+                    tree = tree[n]
+
+    def remove_sub_element(self, scale, position):
+        if self.tree == 1:
+            self.tree = [1,1,1,1,1,1,1,1]
+        elif self.tree == 0:
+            return   #nothing needs to be done!
+        tree = self.tree
+        x, y, z = position
+        print "scale:", scale
+        for i in range(scale,-1,-1):
+            d = 2**i
+            lx = x // d
+            ly = y // d
+            lz = z // d
+            x -= lx*d
+            y -= ly*d
+            z -= lz*d
+            n = lz*4+ly*2+lx
+            print "n=",n
+
+            if i == 0:
+                tree[n] = 0
+            else:
+                if tree[n] == 1:
+                    tree[n] = tmp = [1,1,1,1,1,1,1,1]
+                    tree = tmp
+                elif tree[n] == 0:
+                    exit
+                else:
+                    tree = tree[n]
+
+
+
+
+def draw_octree(surf, block_imgs, offsets, tree, level, maxlevel=4):
+    if type(tree).__name__ == "int":
+        if tree > 0:
+            #print "draw", level, offsets
+            surf.blit(block_imgs[level], (0,0))
+    else:
+        #f = 2**(level+1)
+        wx, hx, wy, hy, hz = [o//2 for o in offsets]
+
+        rect = surf.get_rect()
+        rect.size = (rect.width//2, rect.height//2)
+        for i,tr in enumerate(tree):
+            #rect = surf.get_rect()
+            # a trick using binary representation to get position coordinates
+            z, y, x = [int(x) for x in string.zfill(bin(i)[2:], 3)]
+            ##print x, y, z
+            rect.topleft = (x*wx+(1-y)*wy, y*hy+x*hx+(1-z)*hz)
+            ##print rect.topleft
+            s=surf.subsurface(rect)
+            if level < maxlevel:
+                draw_octree(s, block_imgs, (wx, hx, wy, hy, hz), tr, level+1)
+
+
+
--- game.py
+++ game.py
@@ -20,20 +20,86 @@ class View(object):
         self.size = size #width, height
         self.level = level
         self.position = Vector(*position)
+        self.subcursor = 1
+        self.subcursor_mode=False
+        self.subcurpos = Vector(0,0,0)
 
         self.screen = pygame.display.set_mode(size)
         self.clock = pygame.time.Clock()
 
-        self.block_img = pygame.image.load("block.png")
+        self.block_img = pygame.image.load("block2.png")
         self.block_rect = self.block_img.get_rect()
-        self.block_height = 31
-        self.block_width = 52
-        self.block_depth = 26
+        self.block_hz = 32
+        self.block_wx = 16
+        self.block_hx = 16
+        self.block_wy = 32
+        self.block_hy = 8
 
-        self.cursor_img = pygame.image.load("cursor.png")
+        self.cursor_imgs = [pygame.image.load("cursor2_empty.png"),
+                            pygame.image.load("cursor2b_empty.png"),
+                            pygame.image.load("cursor2c_empty.png"),
+                            pygame.image.load("cursor2d_empty.png")]
+        for img in self.cursor_imgs:
+            img.set_alpha(100)
+        self.cursor_full_img = pygame.image.load("cursor2_full.png")
         self.cursor_rect = self.block_img.get_rect()
 
         self.font = pygame.font.Font(None, 25)
+
+    def get_subcursor_sizes(self):
+        d = 2**(self.subcursor)
+        return (self.block_wx//d, self.block_hx//d, self.block_wy//d, self.block_hy//d, self.block_hz//d)
+
+    def move_position(self, dx=0, dy=0, dz=0):
+        if 0 <= self.position.x+dx < len(self.level.data[0][0]):
+            self.position.x += dx
+        if 0 <= self.position.y+dy < len(self.level.data[0]):
+            self.position.y += dy
+        if 0 <= self.position.z+dz < len(self.level.data):
+            self.position.z += dz
+
+
+    def move_subcursor(self, dx=0, dy=0, dz=0):
+        scale = 2**(self.subcursor)
+        if 0 <= self.subcurpos.x+dx < scale:
+            self.subcurpos.x += dx
+        elif self.subcurpos.x+dx < 0:
+            self.subcurpos.x = scale-1
+            self.move_position(dx=-1)
+        elif self.subcurpos.x+dx >= scale:
+            self.subcurpos.x = 0
+            self.move_position(dx=1)
+
+        if 0 <= self.subcurpos.y+dy < scale:
+            self.subcurpos.y += dy
+        elif self.subcurpos.y+dy < 0:
+            self.subcurpos.y = scale-1
+            self.move_position(dy=-1)
+        elif self.subcurpos.y+dy >= scale:
+            self.subcurpos.y = 0
+            self.move_position(dy=1)
+
+        if 0 <= self.subcurpos.z+dz < scale:
+            self.subcurpos.z += dz
+        elif self.subcurpos.z+dz < 0:
+            self.subcurpos.z = scale-1
+            self.move_position(dz=-1)
+        elif self.subcurpos.z+dz >= scale:
+            self.subcurpos.z = 0
+            self.move_position(dz=1)
+
+
+    def increase_subcursor_scale(self):
+        if self.subcursor+1 <= 3:
+            self.subcursor += 1
+            self.subcurpos = self.subcurpos * 2
+
+    def decrease_subcursor_scale(self):
+        if self.subcursor-1 >= 0:
+            self.subcursor -= 1
+            self.subcurpos.x = self.subcurpos.x // 2
+            self.subcurpos.y = self.subcurpos.y // 2
+            self.subcurpos.z = self.subcurpos.z // 2
 
     def draw(self):
         cx, cy = self.get_screen_center()
@@ -42,17 +108,33 @@ class View(object):
         for z in range(self.level.zsize):
             for y in range(self.level.ysize):
                 for x in range(self.level.xsize):
-                    if self.level.data[z][y][x] == 1:
-                        rect=self.block_rect
-                        rect.center = (cx-self.block_width//2*(posx-posy-x+y),
-                                       cy-self.block_depth//2*(posy+posx-x-y)-self.block_height*(z-posz))
-                        self.screen.blit(self.block_img, rect)
+                    block = self.level.get_block((x, y,z))
+                    rect=self.block_rect
+                    #rect.center = (cx-self.block_width//2*(posx-posy-x+y),
+                    #               cy-self.block_depth//2*(posy+posx-x-y)-self.block_height*(z-posz))
+                    rect.center = (cx+self.block_wx*(x-posx)-self.block_wy*(y-posy),
+                                   cy+self.block_hy*(y-posy)+self.block_hx*(x-posx)-self.block_hz*(z-posz))
+                    if self.screen.get_rect().contains(rect):
+                        block.draw(self.screen.subsurface(rect))
                     if (x, y, z) == (self.position.x, self.position.y, self.position.z):
-                        print "drawing cursor"
-                        rect.center = (cx-self.block_width//2*(posx-posy-x+y),
-                                       cy-self.block_depth//2*(posy+posx-x-y)-self.block_height*(z-posz))
-                        self.screen.blit(self.cursor_img, rect)
-        #print v.position
+                        ##print "drawing cursor"
+                        rect.center = (cx-self.block_wx*(posx-x)+self.block_wy*(posy-y),
+                                        cy-self.block_hy*(posy-y)+self.block_hx*(posx-x)-self.block_hz*(z-posz))
+
+                        self.screen.blit(self.cursor_imgs[0], rect)
+                        if self.subcursor > 0:
+                            xsub, ysub, zsub = self.subcurpos.tuple()
+                            wx, hx, wy, hy, hz = self.get_subcursor_sizes()
+                            ###print "rect before:", rect.topleft
+                            rect =  rect.move(xsub*wx+(2**self.subcursor-1-ysub)*wy,
+                                              ysub*hy+xsub*hx+(2**self.subcursor-1-zsub)*hz)
+                            ###print "rect after:", rect.topleft
+                            ##print "subcursor:", (x*wx+(2**self.subcursor-1-y)*wy, y*hy+x*hx+(2**self.subcursor-1-z)*hz)
+
+                            self.screen.blit(self.cursor_imgs[self.subcursor], rect)
+
+
+        ###print v.position
         postext = self.font.render("%d, %d, %d"%v.position.tuple(), 1, (255,255,0))
         self.screen.blit(postext, (10,10))
         pygame.display.flip()
@@ -62,41 +144,83 @@ class View(object):
 
 lv = data.Level(size=(10,10,10))
 
+b = data.Block(1)
+
 for i in range(10):
-    lv.data[0][0][i] = 1
-    lv.data[0][2][i] = 1
-    lv.data[0][i][0] = 1
-    lv.data[1][i][0] = 1
+    lv.put_block((i,0,0), b)
+    lv.put_block((i,2,0), b)
+    lv.put_block((0,i,0), b)
+    lv.put_block((1,i,1), b)
+
+c=data.Block([1,1,1,0,[1,1,1,0,[1,1,1,0,1,0,0,0],0,0,0],0,0,0])
+lv.put_block((3,3,0), c)
 
 #for i in range(100):
 #    d.set_random_block(1)
 
 v = View(level=lv)
 v.draw()
+
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         elif event.type == pygame.KEYDOWN:
-            print event.key
+            ##print event.key
 
-            if event.key == pygame.K_RIGHT:
-                v.position.x = constrain(v.position.x+1, 0, lv.xsize-1)
-            elif event.key == pygame.K_LEFT:
-                v.position.x = constrain(v.position.x-1, 0, lv.xsize-1)
-            elif event.key == pygame.K_DOWN:
-                v.position.y = constrain(v.position.y+1, 0, lv.ysize-1)
+            if event.key == pygame.K_DOWN:
+                if not v.subcursor_mode:
+                    v.position.x = constrain(v.position.x+1, 0, lv.xsize-1)
+                else:
+                    v.move_subcursor(dx=1)
             elif event.key == pygame.K_UP:
-                v.position.y = constrain(v.position.y-1, 0, lv.ysize-1)
+                if not v.subcursor_mode:
+                    v.position.x = constrain(v.position.x-1, 0, lv.xsize-1)
+                else:
+                    v.move_subcursor(dx=-1)
+            elif event.key == pygame.K_LEFT:
+                if not v.subcursor_mode:
+                    v.position.y = constrain(v.position.y+1, 0, lv.ysize-1)
+                else:
+                    v.move_subcursor(dy=1)
+            elif event.key == pygame.K_RIGHT:
+                if not v.subcursor_mode:
+                    v.position.y = constrain(v.position.y-1, 0, lv.ysize-1)
+                else:
+                    v.move_subcursor(dy=-1)
             elif event.key == pygame.K_PAGEUP:
-                v.position.z = constrain(v.position.z+1, 0, lv.zsize-1)
+                if not v.subcursor_mode:
+                    v.position.z = constrain(v.position.z+1, 0, lv.zsize-1)
+                else:
+                    v.move_subcursor(dz=1)
             elif event.key == pygame.K_PAGEDOWN:
-                v.position.z = constrain(v.position.z-1, 0, lv.zsize-1)
+                if not v.subcursor_mode:
+                    v.position.z = constrain(v.position.z-1, 0, lv.zsize-1)
+                else:
+                    v.move_subcursor(dz=-1)
+
+            elif event.key == pygame.K_PLUS:
+                v.decrease_subcursor_scale()
+            elif event.key == pygame.K_MINUS:
+                v.increase_subcursor_scale()
+
 
             elif event.key == pygame.K_SPACE:
-                if lv.get_block(v.position) == 0:
-                    lv.put_block(v.position, 1)
-                else:
-                    lv.put_block(v.position, 0)
+                if not v.subcursor_mode:
+                    if lv.get_block(v.position) == 0:
+                        lv.put_block(v.position, 1)
+                    else:
+                        lv.put_block(v.position, 0)
+
+            elif event.key == pygame.K_a:
+                print v.subcurpos.tuple()
+                lv.get_block(v.position).put_sub_element(v.subcursor-1, v.subcurpos.tuple(), 1)
+
+            elif event.key == pygame.K_s:
+                print v.subcurpos.tuple()
+                lv.get_block(v.position).remove_sub_element(v.subcursor-1, v.subcurpos.tuple())
+
+            elif event.key == pygame.K_RETURN:
+                v.subcursor_mode = not v.subcursor_mode
 
             v.draw()
 
