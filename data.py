6026aacf6e847558b62fd0c13c74268e7a7b1be0
Johan Forsberg <johan@slentrian.org>
2011-03-31 17:25:14 +0200

    back to basic version, added walls

--- data.py
+++ data.py
@@ -1,8 +1,4 @@
-from __future__ import division
-
 from random import randint
-import string
-import pygame
 
 class Level(object):
     def __init__(self, size=(10,10,3)):
@@ -21,184 +17,25 @@ class Level(object):
                 for x in range(self.xsize):
                     self.data[z][y].append(Block())
 
-    def put_random_block(self, value):
-        self.data [randint(0, self.zsize-1)] [randint(0, self.ysize-1)] [randint(0, self.xsize-1)] = value
+    def put_random_block(self, block):
+        self.data [randint(0, self.zsize-1)] [randint(0, self.ysize-1)] [randint(0, self.xsize-1)] = block
 
     def put_block(self, pos, block):
-        if type(pos).__name__ == "tuple":
-            self.data[pos[2]][pos[1]][pos[0]] = block
-        else:
-            self.data[pos.z][pos.y][pos.x] = block
+        self.data[pos.z][pos.y][pos.x] = block
 
     def get_block(self, pos):
-        if type(pos).__name__ == "tuple":
-            return self.data[pos[2]][pos[1]][pos[0]]
-        else:
-            return self.data[pos.z][pos.y][pos.x]
+        return self.data[pos.z][pos.y][pos.x]
 
 
 class Block(object):
     """
+    This is the basic building block of the world
     """
 
-    block_imgs = [pygame.image.load("block2.png"),
-                  pygame.image.load("block2b.png"),
-                  pygame.image.load("block2c.png"),
-                  pygame.image.load("block2d.png")]
-    block_rects = [bl.get_rect() for bl in block_imgs]
-
-    hz = 32
-    wx = 16
-    hx = 16
-    wy = 32
-    hy = 8
-
-    def __init__(self, tree=0, block_imgs=None, block_size=None):
-        self.tree = tree
-        if block_imgs is not None:
-            self.block_imgs = block_imgs
-            self.block_rects = [bl.get_rect() for bl in block_imgs]
-        if block_size is not None:
-            self.wx, self.hx, self.wy, self.hy, self.hz = block_size
-
-        self.surface = pygame.Surface((self.block_rects[0].size), flags=pygame.SRCALPHA)
-        self.tainted = True
-        self.zoom = 1
-        self.rotation = 0
-
-
-    def get_rect(self):
-        surfsize = self.surface.get_size()
-        return pygame.Rect((0,0),(surfsize[0], surfsize[1]))
-
-    def draw(self, zoom, rotation=0):
-        #print zoom
-        if self.tainted or zoom != self.zoom or rotation != self.rotation:
-            if zoom != self.zoom:
-                self.zoom = zoom
-                self.surface = pygame.Surface((self.block_rects[0].width*zoom,
-                                               self.block_rects[0].height*zoom), flags=pygame.SRCALPHA)
-            if self.rotation != rotation:
-                self.rotation = rotation
-            self.surface.fill((0,0,0,0))
-            draw_octree(self.surface, self.block_imgs, (self.wx, self.hx, self.wy, self.hy, self.hz),
-                        self.tree, 0, zoom=zoom, rotation=rotation)
-            self.tainted=False
-
-    def put_sub_element(self, scale, position, kind):
-        if scale == 0:
-            self.tree = 1
-            self.tainted = True
+    def __init__(self, walls=None, floor=None):
+        if walls is None:
+            self.walls = dict()
         else:
-            if self.tree == 0:
-                self.tree = [0,0,0,0,0,0,0,0]
-            elif self.tree == 1:
-                return   #nothing to be done!
-            tree = self.tree
-            x, y, z = position
-            print "scale:", scale
-            for i in range(scale,0,-1):
-                # calculate block index from coordinates
-                d = 2**(i-1)
-                lx = x // d
-                ly = y // d
-                lz = z // d
-                x -= lx*d
-                y -= ly*d
-                z -= lz*d
-                n = lz*4+ly*2+lx
-                #print "n=",n
-
-                if i == 1:
-                    #if all(tree):
-
-                    tree[n] = kind
-                else:
-                    if tree[n] == 0:
-                        tree[n] = tmp = [0,0,0,0,0,0,0,0]
-                        tree = tmp
-                    elif tree[n] == 1:
-                        return
-                    else:
-                        tree = tree[n]
-            self.tainted=True
-
-    def remove_sub_element(self, scale, position):
-        if scale == 0:
-            self.tree = 0
-            self.tainted=True
-        else:
-            if self.tree == 1:
-                self.tree = [1,1,1,1,1,1,1,1]
-            elif self.tree == 0:
-                return   #nothing needs to be done!
-            tree = self.tree
-            x, y, z = position
-            print "scale:", scale
-            for i in range(scale,0,-1):
-                d = 2**(i-1)
-                lx = x // d
-                ly = y // d
-                lz = z // d
-                x -= lx*d
-                y -= ly*d
-                z -= lz*d
-                n = lz*4+ly*2+lx
-                print "n=",n
-
-                if i == 1:
-                    tree[n] = 0
-                else:
-                    if tree[n] == 1:
-                        tree[n] = tmp = [1,1,1,1,1,1,1,1]
-                        tree = tmp
-                    elif tree[n] == 0:
-                        exit
-                    else:
-                        tree = tree[n]
-            self.tainted=True
-
-
-def rotate_nodes(nodes, n):
-    new = nodes[:]
-    for i in range(n):
-        new = [new[1], new[3], new[0], new[2], new[5], new[7], new[4], new[6]]
-    return new
-
-def draw_octree(surf, block_imgs, offsets, tree, level, maxlevel=4, zoom=1, rotation=0):
-    print "draw_octree, rota =", rotation
-    print "draw_octree, zoom =", zoom
-    if zoom == 2:
-        block_imgs = [pygame.transform.scale2x(block_imgs[0]),] + block_imgs
-    if type(tree).__name__ == "int":
-        if tree > 0:
-            #print "draw", level, offsets
-            surf.blit(block_imgs[level], (0,0))
-    else:
-        #f = 2**(level+1)
-        wx, hx, wy, hy, hz = [zoom*o//2 for o in offsets]
-
-        rect = surf.get_rect()
-        rect.size = (rect.width//2, rect.height//2)
-        #for i,tr in enumerate(tree[4-rotation:4]+tree[:4-rotation]+tree[8-rotation:]+tree[4:8-rotation]):
-        #for i,tr in enumerate(tree[rotation:4]+tree[:rotation]+tree[4+rotation:]+tree[4:4+rotation]):
-        for i, tr in enumerate(rotate_nodes(tree, rotation)):
-            #rect = surf.get_rect()
-            # a trick using binary representation to get position coordinates
-            z, y, x = [int(x) for x in string.zfill(bin(i)[2:], 3)]
-            # if rotation == 1:
-            #     x, y = y, 1-x
-            # elif rotation == 2:
-            #     x, y = 1-x, 1-y
-            # elif rotation == 3:
-            #     x, y = 1-y, x
-            ##print x, y, z
-            rect.topleft = (x*wx+(1-y)*wy, y*hy+x*hx+(1-z)*hz)
-            #rect.topleft = ((1-x)*wx+y*wy, y*hy+x*hx+(1-z)*hz)
-            ##print rect.topleft
-            s = surf.subsurface(rect)
-            if level < maxlevel:
-                draw_octree(s, block_imgs, (wx, hx, wy, hy, hz), tr, level+1, rotation=rotation)
-
-
+            self.walls = walls  # walls are numbered in positive direction starting from top
+        self.floor = floor
 
--- game.py
+++ game.py
@@ -8,11 +8,27 @@ from vector import Vector
 # colors
 BLACK = 0,0,0
 
-def constrain(value, minlim, maxlim, cycle=False):
+def constrain(value, minlim, maxlim):
     "Return the value if it lies between max and min inclusive, otherwise return the closest of max ang min."
-    if not cycle:
-        return max(minlim, min(maxlim, value))
+    return max(minlim, min(maxlim, value))
 
+def get_wall_offset(n):
+    if n == 0:
+        return (0,-0.5)
+    if n == 1:
+        return (-0.25, -0.25)
+    if n == 2:
+        return (-0.5, 0)
+    if n == 3:
+        return (-0.25, 0.25)
+    if n == 4:
+        return (0, 0.5)
+    if n == 5:
+        return (0.25, 0.25)
+    if n == 6:
+        return (0.5, 0)
+    if n == 7:
+        return (0.25, -0.25)
 
 pygame.init()
 
@@ -21,294 +37,149 @@ class View(object):
         self.size = size #width, height
         self.level = level
         self.position = Vector(*position)
-        self.subcursor = 0
-        self.subcursor_mode=False
-        self.subcurpos = Vector(0,0,0)
-        self.zoom = 1
-        self.rotation = 0
 
         self.screen = pygame.display.set_mode(size)
         self.clock = pygame.time.Clock()
 
-        # self.block_img = pygame.image.load("block2.png")
-        # self.block_rect = self.block_img.get_rect()
-        # self.block_hz = 32
-        # self.block_wx = 16
-        # self.block_hx = 16
-        # self.block_wy = 32
-        # self.block_hy = 8
-        # self.block_sizes = self.get_block_sizes()
+        self.block_img = pygame.image.load("block.png")
+        self.block_half_img = pygame.image.load("block_half.png")
+        self.block_rect = self.block_img.get_rect()
+        self.block_height = 32
+        self.block_width = 56
+        self.block_depth = 28
 
-        self.cursor_imgs = [pygame.image.load("cursor2_empty.png"),
-                            pygame.image.load("cursor2b_empty.png"),
-                            pygame.image.load("cursor2c_empty.png"),
-                            pygame.image.load("cursor2d_empty.png")]
-        for img in self.cursor_imgs:
-            img.set_alpha(100)
+        self.wall1_img = pygame.image.load("wall1.png")
+        self.wall2_img = pygame.image.load("wall2.png")
+        self.corner_img = pygame.image.load("corner.png")
 
-        self.cursor = data.Block(tree= 1, block_imgs = self.cursor_imgs, block_size = None)
-
-        self.black_img = pygame.Surface(self.cursor_imgs[0].get_size(), flags=pygame.SRCALPHA)
-        self.black_img.fill((0,0,0,10))
-
-        self.cursor_full_img = pygame.image.load("cursor2_full.png")
-        self.cursor_rect = self.cursor_imgs[0].get_rect()
+        self.cursor_img = pygame.image.load("cursor.png")
+        self.cursor_rect = self.block_img.get_rect()
 
         self.font = pygame.font.Font(None, 25)
 
-    def get_block_sizes(self):
-        return [a*self.zoom for a in self.block_hx, self.block_wx, self.block_hy, self.block_wy, self.block_hz]
-
-    def toggle_zoom(self):
-        if self.zoom == 1:
-            self.zoom = 2
-        else:
-            self.zoom = 1
-
-    def get_cursor_position(self):
-        cp = self.position
-        if self.rotation == 0:
-            return cp
-        elif self.rotation == 1:
-            return Vector(cp.y, self.level.xsize-cp.x-1, cp.z)
-        elif self.rotation == 2:
-            return Vector(self.level.xsize-cp.x-1, self.level.ysize-cp.y-1, cp.z)
-        elif self.rotation == 3:
-            return Vector(self.level.ysize-cp.y-1, cp.x, cp.z)
-
-
-    def move_cursor_on_screen(self, dx=0, dy=0, dz=0):
-        if self.rotation == 1:
-            dx, dy = -dy, dx
-        if self.rotation == 2:
-            dx, dy = -dx, -dy
-        if self.rotation == 3:
-            dx, dy = dy, -dx
-        v.position.x = constrain(v.position.x+dx, 0, lv.xsize-1)
-        v.position.y = constrain(v.position.y+dy, 0, lv.ysize-1)
-        v.position.z = constrain(v.position.z+dz, 0, lv.zsize-1)
-
-    def move_subcursor_on_screen(self, dx=0, dy=0, dz=0):
-        if self.rotation == 1:
-            dx, dy = -dy, dx
-        if self.rotation == 2:
-            dx, dy = -dx, -dy
-        if self.rotation == 3:
-            dx, dy = dy, -dx
-        self.move_subcursor(dx, dy, dz)
-
-
-
-    def get_subcursor_sizes(self):
-        d = 2**(self.subcursor)
-        return (self.block_wx//d, self.block_hx//d, self.block_wy//d, self.block_hy//d, self.block_hz//d)
-
-    def move_position(self, dx=0, dy=0, dz=0):
-        if 0 <= self.position.x+dx < len(self.level.data[0][0]):
-            self.position.x += dx
-        if 0 <= self.position.y+dy < len(self.level.data[0]):
-            self.position.y += dy
-        if 0 <= self.position.z+dz < len(self.level.data):
-            self.position.z += dz
-
-
-    def move_subcursor(self, dx=0, dy=0, dz=0):
-        scale = 2**(self.subcursor)
-        if 0 <= self.subcurpos.x+dx < scale:
-            self.subcurpos.x += dx
-        elif self.subcurpos.x+dx < 0:
-            self.subcurpos.x = scale-1
-            self.move_position(dx=-1)
-        elif self.subcurpos.x+dx >= scale:
-            self.subcurpos.x = 0
-            self.move_position(dx=1)
-
-        if 0 <= self.subcurpos.y+dy < scale:
-            self.subcurpos.y += dy
-        elif self.subcurpos.y+dy < 0:
-            self.subcurpos.y = scale-1
-            self.move_position(dy=-1)
-        elif self.subcurpos.y+dy >= scale:
-            self.subcurpos.y = 0
-            self.move_position(dy=1)
-
-        if 0 <= self.subcurpos.z+dz < scale:
-            self.subcurpos.z += dz
-        elif self.subcurpos.z+dz < 0:
-            self.subcurpos.z = scale-1
-            self.move_position(dz=-1)
-        elif self.subcurpos.z+dz >= scale:
-            self.subcurpos.z = 0
-            self.move_position(dz=1)
-
-        self.cursor.remove_sub_element(scale=0, position=(0,0,0))
-        self.cursor.put_sub_element(scale=self.subcursor, position=self.subcurpos.tuple(), kind=1)
-
-
-    def increase_subcursor_scale(self):
-        if self.subcursor+1 <= 3:
-            self.subcursor += 1
-            self.subcurpos = self.subcurpos * 2
-
-        self.cursor.remove_sub_element(scale=0, position=(0,0,0))
-        self.cursor.put_sub_element(scale=self.subcursor, position=self.subcurpos.tuple(), kind = 1)
-
-
-    def decrease_subcursor_scale(self):
-        if self.subcursor-1 >= 0:
-            self.subcursor -= 1
-            self.subcurpos.x = self.subcurpos.x // 2
-            self.subcurpos.y = self.subcurpos.y // 2
-            self.subcurpos.z = self.subcurpos.z // 2
-
-        self.cursor.remove_sub_element(scale=0, position=(0,0,0))
-        self.cursor.put_sub_element(scale=self.subcursor, position=self.subcurpos.tuple(), kind=1)
-
     def draw(self):
         cx, cy = self.get_screen_center()
-
+        posx, posy, posz = self.position.x, self.position.y, self.position.z
         self.screen.fill(BLACK)
+        rect=self.block_rect
         for z in range(self.level.zsize):
             for y in range(self.level.ysize):
                 for x in range(self.level.xsize):
-                    self.draw_block((x, y, z), (cx, cy), rotation=self.rotation)
+                    bl = self.level.get_block(Vector(x, y, z))
+                    # if self.level.data[z][y][x] == 1:
+                    #
+                    #     rect.center = (cx-self.block_width//2*(posx-posy-x+y),
+                    #                    cy-self.block_depth//2*(posy+posx-x-y)-self.block_height*(z-posz))
+                    #     self.screen.blit(self.block_img, rect)
+                    for w in (0,1,7,2):  #6,3,5,4):  # draw walls from the back
+                        if bl.walls.has_key(w):
+                            xfact, yfact = get_wall_offset(w)
+                            xoffs = xfact*rect.width
+                            yoffs = yfact*self.block_depth
+                            rect.center = (cx-self.block_width//2*(posx-posy-x+y)+xoffs,
+                                           cy-self.block_depth//2*(posy+posx-x-y)-self.block_height*(z-posz)+yoffs)
+                            if w in (1,5):
+                                self.screen.blit(self.wall2_img, rect)
+                            elif w in (3,7):
+                                self.screen.blit(self.wall1_img, rect)
+                            else:
+                                self.screen.blit(self.corner_img, rect)
+                    if bl.floor is None:
+                        pass
+                    elif bl.floor <= 0.5:
+                        rect.center = (cx-self.block_width//2*(posx-posy-x+y),
+                                       cy-self.block_depth//2*(posy+posx-x-y)-self.block_height*(z-posz))
+                        self.screen.blit(self.block_half_img, rect)
+                    else:
+                        rect.center = (cx-self.block_width//2*(posx-posy-x+y),
+                                       cy-self.block_depth//2*(posy+posx-x-y)-self.block_height*(z-posz))
+                        self.screen.blit(self.block_img, rect)
 
-        ###print v.position
+                    if (x, y, z) == (self.position.x, self.position.y, self.position.z):
+                        print "drawing cursor"
+                        rect.center = (cx-self.block_width//2*(posx-posy-x+y),
+                                       cy-self.block_depth//2*(posy+posx-x-y)-self.block_height*(z-posz))
+                        self.screen.blit(self.cursor_img, rect)
+        #print v.position
         postext = self.font.render("%d, %d, %d"%v.position.tuple(), 1, (255,255,0))
-        subpostext = self.font.render("%d, %d, %d"%v.subcurpos.tuple(), 1, (0,255,0))
         self.screen.blit(postext, (10,10))
-        self.screen.blit(subpostext, (10,30))
         pygame.display.flip()
-
-    def draw_block(self, index, screencenter, rotation=0):
-        x, y, z = index
-        if rotation==0:
-            rx, ry, rz = x, y, z
-
-        elif rotation==1:
-            rx, ry, rz = self.level.ysize-y-1, x, z
-        elif rotation==2:
-            rx, ry, rz = self.level.xsize-x-1, self.level.ysize-y-1, z
-        elif rotation==3:
-            rx, ry, rz = y, self.level.xsize-x-1, z
-        block = self.level.get_block((rx, ry, rz))
-
-        posx, posy, posz = self.get_cursor_position().tuple() #self.position.x, self.position.y, self.position.z
-        cx, cy = screencenter
-
-        zoom = self.zoom
-        rect = block.get_rect()
-        #rect.center = (cx-self.block_width//2*(posx-posy-x+y),
-        #               cy-self.block_depth//2*(posy+posx-x-y)-self.block_height*(z-posz))
-        rect.center = (cx+zoom*(block.wx*(x-posx)-block.wy*(y-posy)),
-                       cy+zoom*(block.hy*(y-posy)+block.hx*(x-posx)-block.hz*(z-posz)))
-        #rect.size = rect.width*zoom, rect.height*zoom
-        if self.screen.get_rect().contains(rect):
-            block.draw(zoom=zoom, rotation=rotation)
-            self.screen.blit(block.surface, rect)
-
-        if (rx, ry, rz) == self.position.tuple():
-            ##print "drawing cursor"
-            self.cursor.draw(zoom=zoom, rotation=rotation)
-            self.screen.blit(self.cursor.surface, rect)
 
     def get_screen_center(self):
         return self.size[0]//2, self.size[1]//2
 
 lv = data.Level(size=(10,10,10))
 
-b = data.Block(1)
-
 for i in range(10):
-    lv.put_block((i,0,0), b)
-    lv.put_block((i,2,0), b)
-    lv.put_block((0,i,0), b)
-    lv.put_block((1,i,1), b)
+    lv.put_block(Vector(i,0,0), data.Block(walls={0:1}))
+    lv.put_block(Vector(0,2,i), data.Block(walls={7:1}))
+    #lv.put_block(Vector(0,i,0), data.Block(walls={0:1, 1:1, 2:1, 3:1, 4:1, 5:1, 6:1, 7:1}))
+    lv.put_block(Vector(0,i,0), data.Block(walls={1:1, 3:1, 5:1, 7:1}, floor=0.5))
 
-c=data.Block([1,1,1,0,[1,1,1,0,[1,1,1,0,1,0,0,0],0,0,0],0,0,0])
-lv.put_block((3,3,0), c)
 
 #for i in range(100):
 #    d.set_random_block(1)
 
 v = View(level=lv)
 v.draw()
-
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         elif event.type == pygame.KEYDOWN:
-            ##print event.key
+            print event.key
+            bl = lv.get_block(v.position)
+            direction = -1
 
-            if event.key == pygame.K_DOWN:
-                if not v.subcursor_mode:
-                    v.move_cursor_on_screen(dx=1)
-                else:
-                    v.move_subcursor_on_screen(dx=1)
-            elif event.key == pygame.K_UP:
-                if not v.subcursor_mode:
-                    v.move_cursor_on_screen(dx=-1)
-                else:
-                    v.move_subcursor_on_screen(dx=-1)
+            if event.key == pygame.K_RIGHT:
+                v.position.x = constrain(v.position.x+1, 0, lv.xsize-1)
             elif event.key == pygame.K_LEFT:
-                if not v.subcursor_mode:
-                    v.move_cursor_on_screen(dy=1)
-                else:
-                    v.move_subcursor_on_screen(dy=1)
-            elif event.key == pygame.K_RIGHT:
-                if not v.subcursor_mode:
-                    v.move_cursor_on_screen(dy=-1)
-                else:
-                    v.move_subcursor_on_screen(dy=-1)
+                v.position.x = constrain(v.position.x-1, 0, lv.xsize-1)
+            elif event.key == pygame.K_DOWN:
+                v.position.y = constrain(v.position.y+1, 0, lv.ysize-1)
+            elif event.key == pygame.K_UP:
+                v.position.y = constrain(v.position.y-1, 0, lv.ysize-1)
             elif event.key == pygame.K_PAGEUP:
-                if not v.subcursor_mode:
-                    v.move_cursor_on_screen(dz=1)
-                else:
-                    v.move_subcursor(dz=1)
+                v.position.z = constrain(v.position.z+1, 0, lv.zsize-1)
             elif event.key == pygame.K_PAGEDOWN:
-                if not v.subcursor_mode:
-                    v.move_cursor_on_screen(dz=-1)
-                else:
-                    v.move_subcursor(dz=-1)
+                v.position.z = constrain(v.position.z-1, 0, lv.zsize-1)
 
-            # ZOOM
-            elif event.key == pygame.K_PLUS:
-                v.decrease_subcursor_scale()
-            elif event.key == pygame.K_MINUS:
-                v.increase_subcursor_scale()
+            elif event.key == pygame.K_SPACE:
+                if bl.floor == None:
+                    bl.floor = 1
+                elif bl.floor == 0.5:
+                    bl.floor = None
+                elif bl.floor == 1:
+                    bl.floor = 0.5
 
-            # ADD/SUBTRACT
-            elif event.key == pygame.K_a:
-                print v.subcurpos.tuple()
-                lv.get_block(v.position).put_sub_element(v.subcursor, v.subcurpos.tuple(), 1)
-            elif event.key == pygame.K_s:
-                print v.subcurpos.tuple()
-                lv.get_block(v.position).remove_sub_element(v.subcursor, v.subcurpos.tuple())
 
-            # ROTATE
-            elif event.key == pygame.K_q:
-                if v.rotation == 0:
-                    v.rotation = 3
-                else:
-                    v.rotation -= 1
-                print "rotating, -:", v.rotation
-            elif event.key == pygame.K_w:
-                if v.rotation == 3:
-                    v.rotation = 0
-                else:
-                    v.rotation += 1
-                print "rotating, +:", v.rotation
+            elif event.key == pygame.K_0:
+                direction = 0
+            elif event.key == pygame.K_1:
+                direction = 1
+            elif event.key == pygame.K_2:
+                direction = 2
+            elif event.key == pygame.K_3:
+                direction = 3
+            elif event.key == pygame.K_4:
+                direction = 4
+            elif event.key == pygame.K_5:
+                direction = 5
+            elif event.key == pygame.K_6:
+                direction = 6
+            elif event.key == pygame.K_7:
+                direction = 7
 
-            elif event.key == pygame.K_RETURN:
-                v.subcursor_mode = not v.subcursor_mode
+            if direction > -1 and direction in bl.walls:
+                bl.walls[direction]=int(not bl.walls[direction])
+            else:
+                bl.walls[direction]=1
 
-            elif event.key == pygame.K_z:
-                v.toggle_zoom()
-                v.draw()
 
             v.draw()
 
     v.clock.tick(240)
+
+
 
 """
 while 1:
